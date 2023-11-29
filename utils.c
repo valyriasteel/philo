fb2ffb7d0010f25f99f66957e43b63cf2c9043e3
Berat Boşnak <110120659+valyriasteel@users.noreply.github.com>
2023-11-29 19:30:38 +0000

    norm fix

--- utils.c
+++ utils.c
@@ -1,48 +1,46 @@
 #include "philo.h"
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    int i;
-    int res;
-    int pn;
+	int	i;
+	int	res;
+	int	pn;
 
 	pn = 1;
 	res = 0;
 	i = 0;
-
-	while(str[i] <= 32 && str[i])
+	while (str[i] <= 32 && str[i])
 		i++;
-    if(str[i] == '-' || str[i] == '+')
+	if (str[i] == '-' || str[i] == '+')
 	{
-		if(str[i] == '-')
+		if (str[i] == '-')
 			pn = -1;
 		i++;
-		if(str[i] == '+' || str[i] == '-')
-			return(-1);
+		if (str[i] == '+' || str[i] == '-')
+			return (-1);
 	}
-	while(str[i])
+	while (str[i])
 	{
-		if(str[i] >= '0' && str[i] <= '9')
+		if (str[i] >= '0' && str[i] <= '9')
 			res = res * 10 + str[i++] - 48;
 		else
-			return(-1);
+			return (-1);
 	}
-	return(res * pn);
+	return (res * pn);
 }
 
-u32 gettime()
+u32	gettime(void)
 {
-    struct timeval t;
+	struct timeval	t;
 
-    gettimeofday(&t, NULL);
-
-    return (t.tv_sec * 1000 + t.tv_usec / 1000);
+	gettimeofday(&t, NULL);
+	return (t.tv_sec * 1000 + t.tv_usec / 1000);
 }
 
-void print(t_philosophers *philo, char *str)
+void	print(t_philosophers *philo, char *str)
 {
 	pthread_mutex_lock(&philo->pm->printing);
 	if (philo->pm->is_dead == false)
-    	printf("%d %d %s\n", gettime()-philo->pm->time, philo->id, str);
+		printf("%d %d %s\n", gettime(void)-philo->pm->time, philo->id, str);
 	pthread_mutex_unlock(&philo->pm->printing);
 }
